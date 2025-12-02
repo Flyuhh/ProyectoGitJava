ebabe6b5ff669935d86ec785f9a31ab3aea9708d
Enzo Hernandez Grau <enzograu@icloud.com>
2025-12-02 17:18:33 +0100

    Añadida función sumar y llamada desde main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args){
+
         System.out.println("Hola mundo con GIT!");
+        int resultado =sumar(2,3);
+        System.out.println("la suma es: "+resultado);
+    }
+    public static int sumar(int n1, int n2){
+        return n1+n2;
     }
 }
